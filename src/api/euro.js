683bce0a4be291970008f9ba4825e1e331afe27b
Diego Augusto <diego_augusto@msn.com>
2018-12-09 14:14:16 -0300

    mudança na api

--- src/api/euro.js
+++ src/api/euro.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://shadow-accelerator.glitch.me'
+    baseURL: 'https://euro-api.glitch.me'
+    // baseURL: 'http://localhost:1337'
 });
